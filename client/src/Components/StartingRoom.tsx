96ef0bd80a7481911e5ff96617b1b9c04f086edf
Ken Choi <kenward97@hotmail.com>
2024-06-21 22:24:07 +0000

    added category name to show up in lobby

--- client/src/Components/StartingRoom.tsx
+++ client/src/Components/StartingRoom.tsx
@@ -1,22 +1,21 @@
-// Components/StartingRoom.tsx
-import React from 'react';
 import { useLocation } from 'react-router-dom';
-import { Player, Game } from './WaitingRoom';
+import { Player, Game } from './playerContext';
 
 interface LocationState {
   players: Player[];
   game: Game;
 }
 
-const StartingRoom: React.FC = () => {
+export default function StartingRoom() {
   const location = useLocation();
   const state = location.state as LocationState;
   const { players, game } = state;
 
   return (
     <div className="container mt-4">
-      <h2 className="mb-4">Starting Room</h2>
-      <h3>Game: {game.gamePin}</h3>
+      <h2 className="mb-4">The Imposter Game!</h2>
+      <h3>Game Pin: {game.gamePin}</h3>
+      <h3>Category: {game.categoryId}</h3>
       <ul className="list-group">
         {players.map((player, index) => (
           <li key={index} className="list-group-item">
@@ -26,6 +25,4 @@ const StartingRoom: React.FC = () => {
       </ul>
     </div>
   );
-};
-
-export default StartingRoom;
+}
